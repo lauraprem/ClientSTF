19c2a555847f817769e636f148f4ce3f4e5a2644
lauraprem <premillieu.laura@gmail.com>
2014-04-29 17:04:36 +0200

    sendFile

--- ClientSTF/src/Client/Client.java
+++ ClientSTF/src/Client/Client.java
@@ -62,21 +62,41 @@ public class Client
         datagramPacketReception = new DatagramPacket(buffer, buffer.length, adresseServeur, portServeur);
     }
 
-    public void EnvoieDatagram(DatagramPacket d, InetAddress serveur)
+    public void EnvoiData(DatagramPacket d, InetAddress serveur)
     {
-        try
+        while (true)
         {
-            datagramSoket.send(d);
-            CreationDatagramReception(serveur);
-            datagramSoket.receive(datagramPacketReception);
-        } catch (IOException e)
-        {
-            System.err.println(e.getMessage());
-        } catch (SocketTimeoutException e)
-        {
-            System.out.println("time_out dépassé : " + e.getMessage());
+            try
+            {
+                // on envoie
+                datagramSoket.send(d);
+                CreationDatagramReception(serveur);
+                // on receptionne la réponse
+                datagramSoket.receive(datagramPacketReception);
+                byte[] buf = new byte[512];
+                buf = datagramPacketReception.getData();
+                // si on avait envoyé
+                if (d.getData()[1] == 2)
+                {
+                    if (buf[0] == 0 && buf[1] == 4 && 0 == buf[2] && 0 == buf[3])
+                    {
+                        break;
+                    }
+                } else if ((d.getData()[1] == 3))
+                {
+                    if (buf[0] == 0 && buf[1] == 4 && d.getData()[2] == buf[2] && d.getData()[3] == buf[3])
+                    {
+                        break;
+                    }
+                }
+            } catch (SocketTimeoutException e)
+            {
+                System.err.println("time_out dépassé : " + e.getMessage());
+            } catch (IOException e)
+            {
+                System.err.println(e.getMessage());
+            }
         }
-
     }
 
     // -1 = mauvais déroulement; 1 = bon déroulement
@@ -84,32 +104,48 @@ public class Client
     {
         try
         {
+            //vérification des droits et de l'existance du fichier
             if (fichier.canRead())
             {
                 FileReader monFileReader = new FileReader(fichier);
                 byte[] data = new byte[512];
                 String temp = new String();
+
+                // demande pour envoyer un fichier et acknowlege 
                 temp = "02" + fichier.getName() + "0ctet0";
                 data = temp.getBytes("octet");
                 CreationDatagramEnvoie(data, portServeur, serveur);
-                datagramSoket.send(datagramPacketEnvoie);
-                CreationDatagramReception(serveur);
-                datagramSoket.receive(datagramPacketReception);
+                EnvoiData(datagramPacketEnvoie, serveur);
                 portServeur = datagramPacketReception.getPort();
-                // monFileReader.
+
+                // Envoie du fichier
                 data = new byte[512];
                 temp = new String();
+                String buffer = new String();
+                //numérotation des datagrammes
+                int n = 1;
+                
                 for (int i = 0; i < fichier.length(); i++)
                 {
-                    for (int j = 0; j < 512; j++)
+                    for (int j = 0; j < 512 && monFileReader.ready(); j++)
                     {
-                        temp += monFileReader.read();
+                        buffer += monFileReader.read();
                     }
                     monFileReader.mark(i);
+                    if (n < 10)
+                    {
+                        temp = "0" + n + buffer;
+                    } else
+                    {
+                        temp = n + buffer;
+                    }
+                    data = temp.getBytes("octet");
+                    CreationDatagramEnvoie(data, portServeur, serveur);
+                    EnvoiData(datagramPacketEnvoie, serveur);
+                    buffer = new String();
+                    n++;
 
                 }
-                //temp = "0301"+ fichier.
-
             }
 
         } catch (SecurityException ex)
