5d55a7836439342a79c9468a8a99742a6caa7230
FaCoInf <corinne.fagno@gmail.com>
2014-04-29 18:44:51 +0200

    Interface

--- ClientSTF/src/Client/JFrameClientSTF.java
+++ ClientSTF/src/Client/JFrameClientSTF.java
@@ -55,6 +55,7 @@ public class JFrameClientSTF extends javax.swing.JFrame {
         jTextField2 = new javax.swing.JTextField();
         jLabel4 = new javax.swing.JLabel();
         jTextField3 = new javax.swing.JTextField();
+        jButton2 = new javax.swing.JButton();
         jPanel1 = new javax.swing.JPanel();
         jButtonSendfile = new javax.swing.JButton();
         jLabelNomFichier = new javax.swing.JLabel();
@@ -95,7 +96,7 @@ public class JFrameClientSTF extends javax.swing.JFrame {
         jLabelTitre.setName("jLabelTitre"); // NOI18N
 
         jLabelAdresseServer.setFont(new java.awt.Font("Tahoma", 0, 12)); // NOI18N
-        jLabelAdresseServer.setText("Adresse Server :");
+        jLabelAdresseServer.setText("Adresse Serveur :");
         jLabelAdresseServer.setName("jLabelAdresseServer"); // NOI18N
 
         jTextFieldAdressServer.setFont(new java.awt.Font("Tahoma", 0, 12)); // NOI18N
@@ -114,19 +115,20 @@ public class JFrameClientSTF extends javax.swing.JFrame {
         });
 
         jLabel2.setFont(new java.awt.Font("Tahoma", 0, 12)); // NOI18N
-        jLabel2.setText("Nom du fichier recupéré :");
+        jLabel2.setText("Nom du fichier distant :");
 
         jLabel3.setFont(new java.awt.Font("Tahoma", 0, 12)); // NOI18N
-        jLabel3.setText("Nom du fichier enregistré :");
-
-        jTextField1.setText("jTextField1");
-
-        jTextField2.setText("jTextField2");
+        jLabel3.setText("Nom du fichier local :");
 
         jLabel4.setFont(new java.awt.Font("Tahoma", 0, 12)); // NOI18N
-        jLabel4.setText("jLabel4");
+        jLabel4.setText("Chemin d'enregistrement :");
 
-        jTextField3.setText("jTextField3");
+        jButton2.setText("Select.");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
         jPanel2.setLayout(jPanel2Layout);
@@ -135,23 +137,22 @@ public class JFrameClientSTF extends javax.swing.JFrame {
             .addGroup(jPanel2Layout.createSequentialGroup()
                 .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(jPanel2Layout.createSequentialGroup()
-                        .addGap(155, 155, 155)
-                        .addComponent(jButtonReceivefile))
-                    .addGroup(jPanel2Layout.createSequentialGroup()
-                        .addContainerGap()
-                        .addComponent(jLabel2)
-                        .addGap(18, 18, 18)
-                        .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 157, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addGroup(jPanel2Layout.createSequentialGroup()
                         .addContainerGap()
                         .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addComponent(jLabel3)
-                            .addComponent(jLabel4))
+                            .addComponent(jLabel4)
+                            .addComponent(jLabel2))
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                        .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(jTextField3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, 159, javax.swing.GroupLayout.PREFERRED_SIZE))))
-                .addContainerGap(114, Short.MAX_VALUE))
+                        .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 157, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jTextField2, javax.swing.GroupLayout.DEFAULT_SIZE, 159, Short.MAX_VALUE)
+                            .addComponent(jTextField3))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(jButton2))
+                    .addGroup(jPanel2Layout.createSequentialGroup()
+                        .addGap(143, 143, 143)
+                        .addComponent(jButtonReceivefile)))
+                .addContainerGap(39, Short.MAX_VALUE))
         );
         jPanel2Layout.setVerticalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -160,18 +161,21 @@ public class JFrameClientSTF extends javax.swing.JFrame {
                 .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel2)
                     .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(24, 24, 24)
+                .addGap(18, 18, 18)
                 .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel3)
                     .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(18, 18, 18)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel4, javax.swing.GroupLayout.PREFERRED_SIZE, 14, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jTextField3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 19, Short.MAX_VALUE)
+                    .addComponent(jLabel4, javax.swing.GroupLayout.PREFERRED_SIZE, 24, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jTextField3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jButton2))
+                .addGap(18, 18, 18)
                 .addComponent(jButtonReceivefile)
-                .addContainerGap())
+                .addContainerGap(20, Short.MAX_VALUE))
         );
+
+        jLabel2.getAccessibleContext().setAccessibleName("");
 
         jTabbedPane1.addTab("Recevoir un fichier", jPanel2);
 
@@ -192,7 +196,7 @@ public class JFrameClientSTF extends javax.swing.JFrame {
         jTextFieldNomFichier.setFont(new java.awt.Font("Tahoma", 0, 12)); // NOI18N
         jTextFieldNomFichier.setName("jTextFieldNomFichier"); // NOI18N
 
-        jButtonSelect.setText("Select");
+        jButtonSelect.setText("Select.");
         jButtonSelect.setToolTipText("");
         jButtonSelect.setName("jButtonSelect"); // NOI18N
         jButtonSelect.addActionListener(new java.awt.event.ActionListener() {
@@ -217,7 +221,7 @@ public class JFrameClientSTF extends javax.swing.JFrame {
                     .addGroup(jPanel1Layout.createSequentialGroup()
                         .addGap(149, 149, 149)
                         .addComponent(jButtonSendfile)))
-                .addContainerGap(62, Short.MAX_VALUE))
+                .addContainerGap(58, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -274,7 +278,7 @@ public class JFrameClientSTF extends javax.swing.JFrame {
     private void jButtonSendfileActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButtonSendfileActionPerformed
         if (fichier != null) {
             try {
-                InetAddress AdressServeur = InetAddress.getByName(jTextFieldNomFichier.getText().toString());
+                InetAddress AdressServeur = InetAddress.getByName(jTextFieldAdressServer.getText().toString());
                 client.SendFile(fichier, AdressServeur);
             } catch (UnknownHostException ex) {
                 Logger.getLogger(JFrameClientSTF.class.getName()).log(Level.SEVERE, null, ex);
@@ -293,8 +297,8 @@ public class JFrameClientSTF extends javax.swing.JFrame {
         // Selection du fichier
         JFileChooser jFileChooser = new JFileChooser();
 
-        //int returnVal = jFileChooser.showOpenDialog(this);
-/*
+        int returnVal = jFileChooser.showOpenDialog(this);
+
         if (returnVal == JFileChooser.APPROVE_OPTION) {
             fichier = jFileChooser.getSelectedFile();
 
@@ -302,11 +306,14 @@ public class JFrameClientSTF extends javax.swing.JFrame {
             jTextFieldNomFichier.setText(fichier.getAbsolutePath());
         } else {
             fichier = null;
-        }*/
+        }
+    }//GEN-LAST:event_jButtonSelectActionPerformed
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
         jFileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
         int returnVal = jFileChooser.showOpenDialog(this);
         jTextFieldNomFichier.setText(jFileChooser.getSelectedFile().getAbsolutePath().toString());
-    }//GEN-LAST:event_jButtonSelectActionPerformed
+    }//GEN-LAST:event_jButton2ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -345,6 +352,7 @@ public class JFrameClientSTF extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
     private javax.swing.JButton jButtonReceivefile;
     private javax.swing.JButton jButtonSelect;
     private javax.swing.JButton jButtonSendfile;
